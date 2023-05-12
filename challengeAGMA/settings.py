7599688463d47ca55b5a63abbd3d445677b29a20
Gustavo Camargo <camargogustavoa@gmail.com>
2023-05-11 23:29:19 -0500

    Fifth Attempt for static

--- challengeAGMA/settings.py
+++ challengeAGMA/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/staticfiles'),]
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 
 
 # Default primary key field type
